ca6a571e56c4ed7c52ffdad949c4525e59b05de7
Marcus Ong <marcus.ong.2014@sis.smu.edu.sg>
2018-03-19 18:23:01 +0800

    Added embedded google maps

---
+++ app/src/main/java/com/teampark/globalstudiossingapore/Entity/Records.java
@@ -0,0 +1,29 @@
+package com.teampark.globalstudiossingapore.Entity;
+
+/**
+ * Created by Mong on 19/3/18.
+ */
+
+public class Records {
+
+    private String records_id;
+    private String beacon_id;
+    private int isEntered;
+    private String time;
+
+    public String getRecords_id() {
+        return records_id;
+    }
+
+    public String getBeacon_id() {
+        return beacon_id;
+    }
+
+    public int getIsEntered() {
+        return isEntered;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
