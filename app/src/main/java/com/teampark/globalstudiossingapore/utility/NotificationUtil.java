94e39b257fd664b1c6bcd9b5c6fd18538002ab3d
Darryl <ongrongsheng@gmail.com>
2018-03-27 17:24:31 +0800

    Added Notifications

---
+++ app/src/main/java/com/teampark/globalstudiossingapore/utility/NotificationUtil.java
@@ -0,0 +1,73 @@
+package com.teampark.globalstudiossingapore.utility;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.support.v4.app.NotificationCompat;
+
+import com.teampark.globalstudiossingapore.MainActivity;
+import com.teampark.globalstudiossingapore.R;
+
+/**
+ * Created by darrylong on 27/3/18.
+ */
+
+public class NotificationUtil {
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    public static void sendNotification(Context context, String messageBody) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = context.getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(context, channelId)
+                        .setSmallIcon(R.drawable.rollercoaster)
+                        .setContentTitle("Global Studios SG")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    public static void countdownNotification(Context context, String messageBody, int timeInSeconds){
+        new CountDownTimer(timeInSeconds * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+//                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                sendNotification(context, messageBody);
+
+            }
+        }.start();
+    }
+}
